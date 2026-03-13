baa4e8d5c454721d939093d949d8044f6ab6794d
RobotoSkunk <contact@robotoskunk.com>
2026-03-13 14:04:17 -0600

    Created tables

--- queries.sql
+++ queries.sql
@@ -1,6 +1,23 @@
+-- MSSQL
 
 CREATE DATABASE sql_challenge_c;
 USE sql_challenge_c;
 
 
+-- Create tables
+CREATE TABLE HardwareComponents (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Price DECIMAL(7, 2) NOT NULL,
+	IsSubassembly BIT NOT NULL
+);
 
+CREATE TABLE PcParts(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	ComponentId INT NOT NULL,
+	Quantity INT NOT NULL DEFAULT 1,
+	ParentId INT,
+
+	FOREIGN KEY (ComponentId) REFERENCES HardwareComponents(Id),
+	FOREIGN KEY (ParentId) REFERENCES PcParts(Id)
+);
